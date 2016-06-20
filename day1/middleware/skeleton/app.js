3c18f90bd41255b56a4d446b8eadca471c2f5745
Virginia Van Keuren <virginiavankeuren2017@u.northwestern.edu>
2016-06-20 11:54:46 -0400

    added Lane's notes

--- day1/middleware/skeleton/app.js
+++ day1/middleware/skeleton/app.js
@@ -13,22 +13,31 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//all of these have next in it, so they pass down to each line of code
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// this loads the routes file
+// if the page is found in the routes document, it goes to it, then terminates it. Doesn't call next.
+//if the page isn't found, it goes to the next line of code, the 404 function
 app.use('/', routes);
 
 // catch 404 and forward to error handler
+//404 means not found, this is a catch-all
+//router.get = retrieves the route /
+//app.use does all (get, post, etc)
+//this function calls everything (everything goes through this function)
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  next(err);//how that middleware tells them how to move on to the next layer, the next middleware (without it it'll stop)
+  //res.render('index', {title:ERROR});
 });
 
-// error handlers
+// error handlers (has 4 variables)
 
 // development error handler
 // will print stacktrace
--- day1/middleware/skeleton/routes/index.js
+++ day1/middleware/skeleton/routes/index.js
@@ -6,22 +6,56 @@ var user = null;
 
 // Your middleware goes here.
 // CAREFUL! Order matters!
+var myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
+};
+
+router.use(myLogger);
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  // next();
 });
 
 router.get('/hidden', function(req, res, next) {
-  res.send("You found me! Drat!");
+  res.send("Access Denied");
+  // next();
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login');
+  // next();
 });
 
 router.post('/login', function(req, res, next) {
   // Your code here
   res.send("Not implemented yet");
+  // next();
 });
 
 module.exports = router;
+
+//NOTES from lecture
+//get only returns get
+// router.get('/', function(req,res,next){
+//   console.log('MATCHED ROOT ROUTE');
+//   res.render('index',{title:'Express'});
+//   // res.render('error', {message: "I win",})
+//   // var err = new Error('Not Found');
+//   // err.message = "Hi there";
+//   res.redirect('/moose123'); //this redirects to a specific page, not like next (which goes to next function)
+// });
+//
+// router.use('/moose123', function(req,res,next){
+//   console.log('Time', Date.now());
+//   res.send(err.message);
+// })
+//JS is stateful: it remembers whta's in the console
+//HTTP is stateLESS: p stands for protocol, has no memory
+//this will work for all users (not people individually)
+var x = 0;
+router.get('/', function(req, res, next) {
+  res.render('index', { title: x++ });
+  // next();
+});
