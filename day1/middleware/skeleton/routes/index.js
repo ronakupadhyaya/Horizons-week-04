f6853841af78cec6fd61f32f40506db5bc56d250
Thomas Eng <tomeng728@gmail.com>
2016-06-20 15:57:08 -0400

    Commit Week 4 Day 1

--- day1/middleware/skeleton/routes/index.js
+++ day1/middleware/skeleton/routes/index.js
@@ -7,6 +7,21 @@ var user = null;
 // Your middleware goes here.
 // CAREFUL! Order matters!
 
+router.use(function (req, res, next) {
+	console.log('Hello World!');
+	next();
+});
+
+router.use('/hidden', function(req, res, next) {
+	if (user) next();
+	else {
+		res.redirect('/login?redirect=hidden');
+	}
+});
+
+// var handleHidden = function(req, res, next) {
+// }; -----> pass this into router.get('/hidden', handleHidden, function(req, res, next) { .... })
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -21,7 +36,12 @@ router.get('/login', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   // Your code here
-  res.send("Not implemented yet");
+  user = req.body.username;
+  if (req.query.redirect) {
+  	res.redirect('/' + redirect);
+  } else {
+  	res.redirect('/');
+  }
 });
 
 module.exports = router;
