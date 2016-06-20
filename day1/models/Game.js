31228a2670feadc5c08ae53aed87e324cd2dee42
Ethan Lee <ethanlee@ethansites.com>
2016-06-19 23:19:37 -0400

    Knock out models

--- day1/models/Game.js
+++ day1/models/Game.js
@@ -12,81 +12,34 @@ var GameSchema = new mongoose.Schema({
   houseHand: [],
 });
 
-GameSchema.statics.newGame = function (item, callback){
+GameSchema.statics.newGame = function(item, callback){
   var game = new this(item)
   game.deck = new Deck();
   game.save(callback);
 }
 
-GameSchema.statics.deal21 = function (game) {
-  game.currentPlayerHand=[];
-  game.houseHand=[];
-  for(var i=0; i<2; i++){
-    game.currentPlayerHand.push(game.deck.pop());
-    game.houseHand.push(game.deck.pop());
-  }
-  game.userTotal = this.calcValue(game.currentPlayerHand);
-  game.dealerTotal = this.calcValue(game.houseHand);
+GameSchema.statics.deal21 = function(game) {
+  // YOUR CODE HERE
 };
 
-GameSchema.statics.calcValue = function (hand){
-  var val = 0;
-  var tempArr = hand;
-  tempArr.sort(function(a,b) { return parseFloat(a.val) - parseFloat(b.val) } );
-  for(var i=tempArr.length-1; i>=0; i--) {
-    var temp = tempArr[i];
-    if(temp.val === 1 && val <=10){temp.val = 11;}
-    else if(temp.val >=10){temp.val = 10;}
-    val += temp.val;
-  }
-  return val;
+GameSchema.statics.calcValue = function(hand){
+  // YOUR CODE HERE
 }
 
-GameSchema.statics.hit = function (game){
-  game.currentPlayerHand.push(game.deck.pop());
-  game.userTotal = this.calcValue(game.currentPlayerHand);
-  if(parseInt(game.userTotal) > 21){
-    console.log("asd")
-    game.userStatus = "lost";
-    this.gameOver(game);
-  }
+GameSchema.statics.hit = function(game){
+  // YOUR CODE HERE
 };
 
 GameSchema.statics.stand = function stand(game){
-  while(game.dealerTotal < 17){
-    game.houseHand.push(game.deck.pop());
-    game.dealerTotal = this.calcValue(game.houseHand);
-    if(game.dealerTotal > 21){
-      game.dealerStatus = "lost";
-    }
-  }
-  this.gameOver(game);
+  // YOUR CODE HERE
 }
 
 GameSchema.statics.gameOver = function gameOver(game){
-  game.status="over";
-  if(game.userTotal > game.dealerTotal && game.userStatus !== "lost" || game.dealerStatus === "lost"){
-    game.userStatus= "won";
-    game.dealerStatus= "lost";
-    //this.money+=2; // TODO += 2*bet
-    // RESPONSE YOU WIN
-  }
-  else if(game.userTotal === game.dealerTotal && game.userStatus !== "lost"){
-    console.log("HAH you tied.")
-    game.dealerStatus= "tied";
-    game.userStatus= "tied";
-    //this.money++; // money += bet.
-    //response -> TIED
-  }else{
-    game.userStatus= "lost";
-    game.dealerStatus= "won";
-  }
+  // YOUR CODE HERE
 }
 
 function Card(suit, val, symbol) {
-  this.suit = suit;
-  this.val = val;
-  this.symbol = symbol;
+  // YOUR CODE HERE
 }
 
 function Deck(){
@@ -96,37 +49,12 @@ function Deck(){
   return this.deck;
 }
 
-Deck.prototype.createDeck = function (){
-  var suit, symbol;
-  for(var k=1; k<=4; k++){
-    switch(k){
-      case 1: suit ="hearts"; break;
-      case 2: suit ="diamonds"; break;
-      case 3: suit ="spades"; break;
-      case 4: suit ="clubs"; break;
-    }
-    for(var i=1; i<=13; i++){
-      symbol = i;
-      switch(i){
-        case 1: symbol = "A"; break;
-        case 11: symbol = "J"; break;
-        case 12: symbol = "Q"; break;
-        case 13: symbol = "K"; break;
-      }
-      this.deck.push(new Card(suit, i, symbol));
-    }
-  }
+Deck.prototype.createDeck = function() {
+  // YOUR CODE HERE
 }
 
-Deck.prototype.shuffleDeck = function () {
-  var currentIndex = this.deck.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = this.deck[currentIndex];
-    this.deck[currentIndex] = this.deck[randomIndex];
-    this.deck[randomIndex] = temporaryValue;
-  }
+Deck.prototype.shuffleDeck = function() {
+  // YOUR CODE HERE
 }
 
-module.exports  =  mongoose.model('Game', GameSchema);
+module.exports = mongoose.model('Game', GameSchema);
