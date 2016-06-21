628a4ae37fe99f1233da6a61d64459dbb60cfc6e
Lucas <lmandelbaum1@babson.edu>
2016-06-21 13:00:27 -0400

    day2 warmup

--- day2/sendgrid/skeleton/index.js
+++ day2/sendgrid/skeleton/index.js
@@ -1,1 +1,21 @@
 // Your code here!
+
+  var helper = require('sendgrid').mail
+  from_email = new helper.Email("lucasmandelbaum@gmail.com")
+  to_email = new helper.Email("lmandelbaum1@babson.com")
+  subject = "Hello World from the SendGrid Node.js Library"
+  content = new helper.Content("text/plain", "some text here")
+  mail = new helper.Mail(from_email, subject, to_email, content)
+  var apiKey = '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'
+
+var sg = require('sendgrid').SendGrid(process.env.apiKey)
+
+// GET Collection
+var request = sg.emptyRequest()
+request.method = 'GET'
+request.path = '/v3/api_keys'
+sg.API(request, function (response) {
+  console.log(response.statusCode)
+  console.log(response.body)
+  console.log(response.headers)
+})
