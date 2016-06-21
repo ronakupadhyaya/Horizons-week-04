ef984c61fb8545d06c4cec568c694dc2c4e70da0
Isaac Kapadia <ikapedia@umich.edu>
2016-06-20 22:37:59 -0400

    worked on blackjack

--- day1/blackjack/models/Game.js
+++ day1/blackjack/models/Game.js
@@ -1,20 +1,55 @@
 var mongoose = require('mongoose');
+mongoose.connect(require('./config'))
 
 var GameSchema = new mongoose.Schema({
-  // YOUR CODE HERE
+  bet: {
+    type: Number,
+    required: true
+  },
+  playerHand: [],
+
+  dealerHand: [],
+
+  deckCards: [],
+
+  playerValue: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  dealerValue: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  gameStatus: {
+    type: String,
+    default: 'not started',
+    required: true
+  },
+  playerStatus: {
+    type: String,
+    required: true
+  },
+  dealerStatus: {
+    type: String,
+    required: true
+  }
 });
 
-GameSchema.statics.newGame = function(item, callback){
+GameSchema.statics.newGame = function(item, callback) {
   var game = new this(item)
   game.deck = new Deck();
   game.save(callback);
 }
 
 function Card(suit, val, symbol) {
-  // YOUR CODE HERE
+  this.suit = suit,
+  this.val = val,
+  this.symbol = symbol
 }
 
-function Deck(){
+function Deck() {
   this.deck = [];
   this.createDeck()
   this.shuffleDeck()
@@ -22,27 +57,113 @@ function Deck(){
 }
 
 Deck.prototype.createDeck = function() {
-  // YOUR CODE HERE
-}
+
+  var suitArray = ['hearts', 'diamonds', 'clubs', 'spades'];
+  var symbolArray = ['A', 'K', 'Q', 'J'];
+
+  // add number cards to deck
+  for (var i = 2; i < 11; i++) {
+    for (var j = 0; j < suitArray.length; j++) {
+      this.deck.push(Card(suitArray[j], i, i));
+    }
+  }
+
+  // add picture cards to deck
+  for (var j = 0; j < suitArray.length; j++) {
+    for (var k = 0; k < symbolArray.length; k++) {
+      if (k === 0) {
+        this.deck.push(Card(suitArray[j], 'A', 11));
+      }
+      else {
+      this.deck.push(Card(suitArray[j], symbolArray[k], 10));
+      }
+    }
+  }
+}  
+        
 
 Deck.prototype.shuffleDeck = function() {
-  // YOUR CODE HERE
+  
+  // Looked up Fisher-Yates algorithm for card shuffling
+  for (var i = this.deck.length - 1; i > 0; i--) {
+    var n = Math.floor(Math.random() * i);
+    var a = this.deck[n];
+    this.deck[n] = this.deck[i];
+    this.deck[i] = a;
+  }
 }
 
-GameSchema.methods.calcValue = function(hand){
-  // YOUR CODE HERE
+GameSchema.methods.calcValue = function(hand) {
+  // calculate both player and dealer hand values
+  var handValue = 0;
+  for (var i = 0; i < hand.length; i++) {
+    if (hand[i].val === 11) {
+      if (handValue <= 10) {
+        handValue += 11;
+      }
+      else {
+        handValue == 1;
+      }
+    }
+    else {
+    handValue += hand[i].val;
+    }
+  }
 }
 
 GameSchema.methods.dealInitial = function() {
-  // YOUR CODE HERE
+
+  this.deckCards = new Deck();
+
+  // add 2 cards from the deck to the player's hand and 2 cards to the dealer's hand
+  for (var i = 0; i < 2; i++) {
+    this.playerHand += this.deckCards[this.deckCards.length];
+    this.deckCards.pop();
+    this.dealerHand += this.deckCards[this.deckCards.length];
+    this.deckCards.pop();
+  }
+
+  // calculate the player's starting value and dealer's starting value
+  this.playerValue = this.calcValue(this.playerHand);
+  this.dealerValue = this.calcValue(this.dealerHand);
+
+  // update the game status
+  this.gameStatus = 'first deal';
 };
 
 GameSchema.methods.hit = function(){
-  // YOUR CODE HERE
+  
+  // add card to player's hand
+  this.playerHand += this.deckCards[this.deckCards.length];
+  this.deckCards.pop();
+
+  // calculate player's value
+  this.playerValue = this.calcValue(this.playerHand);
+ 
+  //  check if player value exceeds 21 or is equal to 21
+  if (this.playerValue > 21) {
+    this.gameStatus = 'game over, player loses';
+  }
+  else if (this.playerValue === 21) { 
+    if (this.dealerValue === 21) {
+      // player gets bets back
+    }
+    else {
+      // player wins
+      this.gameStatus = 'game over, player wins';
+    }
+  }
 };
 
 GameSchema.methods.stand = function(){
-  // YOUR CODE HERE
+  
+  this.dealerHand += this.deckCards[this.deckCards.length];
+  this.deckCards.pop();
+
+  this.dealerValue = this.calcValue(this.dealerHand);
+
+  // call the function again (recursive)
+  this.stand();
 }
 
 GameSchema.methods.gameOver = function(){
