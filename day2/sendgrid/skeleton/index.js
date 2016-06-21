c446f9c59d4c64d7df5aa65989d2cfb5661956dc
tconcannon <taycon@seas.upenn.edu>
2016-06-21 13:02:49 -0400

    working

--- day2/sendgrid/skeleton/index.js
+++ day2/sendgrid/skeleton/index.js
@@ -3,10 +3,10 @@
 var sendgrid= require('sendgrid')
 var api ="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";
   var helper = require('sendgrid').mail
-  from_email = new helper.Email("test@example.com")
+  from_email = new helper.Email("taycon@seas.upenn.edu")
   to_email = new helper.Email("taycon@seas.upenn.edu")
-  subject = "Hello World from the SendGrid Node.js Library"
-  content = new helper.Content("text/plain", "some text here")
+  subject = "I made this-- Taylor"
+  content = new helper.Content("text/plain", "hi it's taylor. sending email from api and code")
   mail = new helper.Mail(from_email, subject, to_email, content)
 
   var sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
