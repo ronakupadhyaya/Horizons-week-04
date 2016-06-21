dfc18912692f1d0e70922a6f4936c0605a3d6493
joostkamermans <JoostKamermans1@gmail.com>
2016-06-21 12:39:27 -0400

    asdf
    sdf

--- day2/sendgrid/skeleton/index.js
+++ day2/sendgrid/skeleton/index.js
@@ -1,1 +1,28 @@
-// Your code here!
+  var helper = require('sendgrid').mail
+  from_email = new helper.Email("joostk@wharton.upenn.edu")
+  to_email = new helper.Email("joostk@wharton.upenn.edu")
+  subject = "Hello World from the SendGrid Node.js Library"
+  content = new helper.Content("text/plain", "some text here")
+  mail = new helper.Mail(from_email, subject, to_email, content)
+
+  var sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
+  var requestBody = mail.toJSON()
+  var request = sg.emptyRequest()
+  request.method = 'POST'
+  request.path = '/v3/mail/send'
+  request.body = requestBody
+  sg.API(request, function (response) {
+    console.log(response.statusCode)
+    console.log(response.body)
+    console.log(response.headers)
+  })// Your code here!
+
+
+
+
+
+
+
+
+// echo "sendgrid.env" >> .gitignore
+// source ./sendgrid.env
