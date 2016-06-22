d3318c2e430b9936c88fd05a6ecaa0082ceac16c
Serena Woodward <smew820@gmail.com>
2016-06-22 09:14:18 -0400

    twill

---
+++ day2/twilio/twilio_warmup.js
@@ -0,0 +1,38 @@
+var twilio = require('twilio');
+//require the Twilio module and create a REST client
+var client = require('twilio')('ACCOUNT_SID', '26be4851c6d2c97a416446e5a86c1802');
+
+//Send an SMS text message
+client.sendMessage({
+
+    to:'+16515556677', // Any number Twilio can deliver to
+    from: '+14506667788', // A number you bought from Twilio and can use for outbound communication
+    body: 'word to your mother.' // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+    }
+});
+
+//Place a phone call, and respond with TwiML instructions from the given URL
+client.makeCall({
+
+    to:'+16515556677', // Any number Twilio can call
+    from: '+14506667788', // A number you bought from Twilio and can use for outbound communication
+    url: 'http://www.example.com/twiml.php' // A URL that produces an XML document (TwiML) which contains instructions for the call
+
+}, function(err, responseData) {
+
+    //executed when the call has been initiated.
+    console.log(responseData.from); // outputs "+14506667788"
+
+});
