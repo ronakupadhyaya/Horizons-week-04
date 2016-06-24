b0a7593967125f490efb00999ed7012dd489bafb
tconcannon <taycon@seas.upenn.edu>
2016-06-24 17:16:34 -0400

    done with cookies and pre-mongo

--- day5/vault/app.js
+++ day5/vault/app.js
@@ -3,8 +3,17 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var userPasswords = require('./passwords.plain.json').passwords;
+// var cookieSession = require('cookie-session');
+var session=require('express-session');
 
 var app = express();
+
+// app.use(cookieSession({keys: ['my secret for signing cookies']}))
+//var user={};
+app.use(session({secret: 'your secret here'}))
 
 // Express setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,14 +33,70 @@ function hashPassword(password) {
 
 // Models are defined in models/models.js
 var models = require('./models/models');
+//var connect= reqquire('./models/connect')
 
 // SET UP PASSPORT HERE
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    console.log(userPasswords.length)
+    for(var i=0; i<userPasswords.length; i++){
+      if(userPasswords[i].username===username && userPasswords[i].password===password){
+        var user=userPasswords[i];
+        return done(null, user)
+      }
+    }
+    return done(null, false)
+  }))
+
+passport.serializeUser(function(user,done){
+  //cookieSession.id=user._id
+  done(null, user._id)
+})
+
+passport.deserializeUser(function(userid,done){
+  for(var i=0; i<userPasswords.length; i++){
+    var user=userPasswords[i];
+    if(user._id===userid){
+      return done(null,user)
+    }
+  }
+})
+
+// app.use(function (req, res, next) {
+//   req.sessionOptions.maxAge = 120000;
+//   return next()
+// })
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.get('/login', function(req,res){
+  res.render('login')
+})
+
+app.post('/login', passport.authenticate('local',{
+  successRedirect: '/',
+  failureRedirect: '/login'
+}))
 
 // GET /: This route should only be accessible to logged in users.
-router.get('/', function(req, res, next) {
+app.use('/', function(req, res, next) {
   // Your code here.
-  res.render('index');
+  if(!req.user){
+    res.redirect('/login')
+  }
+  else{
+    return next()
+    //res.render('index')
+  }
 });
+
+
+app.get('/', function(req,res){
+  res.render('index')
+})
 
 
 // catch 404 and forward to error handler
--- day5/vault/models/models.js
+++ day5/vault/models/models.js
@@ -1,9 +1,18 @@
 "use strict";
 
 var mongoose = require('mongoose');
-var connect = process.env.MONGODB_URI || require('./models/connect');
+var connect = process.env.MONGODB_URI || require('./connect');
 mongoose.connect(connect);
 
 module.exports = {
   // YOUR MODELS HERE
+  username: {
+  	type: String,
+  	required: true
+  },
+  password: {
+  	type: String,
+  	required: true
+  } 
+
 };
