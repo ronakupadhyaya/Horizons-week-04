51d1b7ada8f561ab9749fa8aca0a2eaff3051e15
aschugart <annieschugart@Annies-MacBook-Pro-2.local>
2016-06-24 14:49:29 -0400

    committing

--- day5/hashing/hash.js
+++ day5/hashing/hash.js
@@ -10,9 +10,23 @@
 // We've filled in a very simple, naive (and pretty bad) hash function for you
 // as a starting point.
 
-function hashIt(word) {
+// function hashIt(word) {
   // This hash function returns a string, and it's fast, but it's pretty
   // crummy since it produces tons of collisions!
-  return word.length.toString();
-}
+  // return word.length.toString();
+//   var output = 0;
+// 	for (var i = 0; i < word.length; i++) {
+// 		output += word[i].charCodeAt();
+// 		var output2 = output % 100;
+// 		return output2.toString()
+// 	}
+// }
 
+function hashIt(word) {
+	var output = ""
+	for (var i = 0; i < word.length; i++) {
+		output =+ (word[i].charCodeAt() + word[i]);
+	} 
+	var output2 = output % 100;
+	return output2.toString();
+}
