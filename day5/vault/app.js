b648ff438560f8577cfbec46870a08393a907eaf
Ying-Hang Eng <eyinghang@gmail.com>
2016-06-24 17:23:26 -0400

    finished

--- day5/vault/app.js
+++ day5/vault/app.js
@@ -3,8 +3,15 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var User = require('./models/models').User;
+
+
 
 var app = express();
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+app.use(session({ secret: 'horizonisrad', store: new MongoStore({mongooseConnection: require('mongoose').connection}) }));
+
 
 // Express setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,9 +33,91 @@ function hashPassword(password) {
 var models = require('./models/models');
 
 // SET UP PASSPORT HERE
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var userPasswords = require('./passwords.hashed.json').passwords;
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Tell Passport how to set req.user
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }).select('username password').exec(function (err, user) {
+    // if there's an error, finish trying to authenticate (auth failed)
+    if (err) { 
+      return done(err);
+    }
+    // if no user present, auth failed
+    if (!user) {
+      return done(null, false);
+    }
+    console.log("HERE!")
+    var validPassword = false;
+    var hash = hashPassword(password);
+    if(hash === user.password){
+      validPassword = true;
+    }
+    // if passwords do not match, auth failed
+    if (!validPassword) {
+      return done(null, false);
+    }
+    // auth has has succeeded
+    return done(null, user);
+  });
+}));
+
+app.get('/login', function(req, res, next) {
+  // Your code here.
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+app.post('/signup',function(req, res) {
+      
+  var user = new User();    // create a new instance of the User model
+  user.username = req.body.username;  // set the users username (comes from the request)
+  var hashsave = hashPassword(req.body.password);
+  user.password = hashsave;  // set the users password (comes from the request)
+  user.save(function(err) {
+    if (err) {
+      // duplicate entry
+      console.log(err);
+      res.render('signup', {error: err});
+    }
+
+    // return a message
+    res.redirect('login');
+  });
+
+})
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/login');
+});
+
+app.use(function(req, res, next){
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    return next();
+  }
+});
 
 // GET /: This route should only be accessible to logged in users.
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   // Your code here.
   res.render('index');
 });
--- day5/vault/models/models.js
+++ day5/vault/models/models.js
@@ -1,9 +1,22 @@
 "use strict";
 
 var mongoose = require('mongoose');
-var connect = process.env.MONGODB_URI || require('./models/connect');
+var connect = process.env.MONGODB_URI;
 mongoose.connect(connect);
+
+// console.log when conncted
+var db = mongoose.connection;
+
+db.once('open', function callback () {
+       console.log("DB Connected!");
+});
+
+var UserSchema = new mongoose.Schema({
+	username: { type: String, index: { unique: true }},
+	password: { type: String}
+});
 
 module.exports = {
   // YOUR MODELS HERE
+  User: mongoose.model('User', UserSchema)
 };
