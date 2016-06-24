a559b5ef71e2fbfe3d69df1a8043722d615ef379
Ying-Hang Eng <eyinghang@gmail.com>
2016-06-24 13:32:32 -0400

    hash table

--- day2/sendgrid/skeleton/index.js
+++ day2/sendgrid/skeleton/index.js
@@ -1,1 +1,18 @@
-// Your code here!
+var helper = require('sendgrid').mail
+from_email = new helper.Email("test@example.com")
+to_email = new helper.Email("eyinghang@gmail.com")
+subject = "Hello World from the SendGrid Node.js Library"
+content = new helper.Content("text/plain", "some text here")
+mail = new helper.Mail(from_email, subject, to_email, content)
+
+var sg = require('sendgrid').SendGrid('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')
+var requestBody = mail.toJSON()
+var request = sg.emptyRequest()
+request.method = 'POST'
+request.path = '/v3/mail/send'
+request.body = requestBody
+sg.API(request, function (response) {
+  console.log(response.statusCode)
+  console.log(response.body)
+  console.log(response.headers)
+})
--- day5/hashing/hash.js
+++ day5/hashing/hash.js
@@ -13,6 +13,13 @@
 function hashIt(word) {
   // This hash function returns a string, and it's fast, but it's pretty
   // crummy since it produces tons of collisions!
-  return word.length.toString();
+  var hash = 0, i, chr, len;
+  if (word.length === 0) return hash;
+  for (i = 0, len = word.length; i < len; i++) {
+    chr   = word.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
 }
 
