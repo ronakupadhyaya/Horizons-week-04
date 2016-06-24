8474830aaab65a03b8980ca06db600f8a22e993b
Thomas Eng <tomeng728@gmail.com>
2016-06-24 16:46:49 -0400

    updates

--- day5/vault/app.js
+++ day5/vault/app.js
@@ -3,6 +3,18 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+// var cookieSession = require('cookie-session');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+// app.use(cookieSession({keys: ['my secret for signing cookies']}))
+
+var routes = require('./routes/index');
+var auth = require('./routes/auth');
+var models = require('./models/models');
+var hashPassword = require('./hashPassword');
 
 var app = express();
 
@@ -13,6 +25,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI || require('./models/connect');
+mongoose.connect(connect);
 
 // This is the function we're going to use in Phases 4 and 5 to hash
 // user passwords.
@@ -27,12 +43,75 @@ var models = require('./models/models');
 
 // SET UP PASSPORT HERE
 
+app.use(session({
+  secret: process.env.SECRET,
+  cookie: {
+    // In milliseconds, i.e., five minutes
+    maxAge: 1000 * 60 * 5
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  models.User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+app.use(function(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+});
+
+passport.use(new LocalStrategy(function (username, password, done) {
+    var hash = hashPassword(password);
+
+    // Find the user with the given username
+    models.User.findOne({username: username}, function (err, user) {
+      // if there's an error, finish trying to authenticate (auth failed)
+      if (err) {
+        console.error(err);
+        return done(err);
+      }
+      // if no user present, auth failed
+      if (!user) {
+        console.log(user);
+        return done(null, false, {message: 'Incorrect username.'});
+      }
+      // if passwords do not match, auth failed
+      if (user.password !== hash) {
+        return done(null, false, {message: 'Incorrect password.'});
+      }
+      // auth has has succeeded
+      return done(null, user);
+    });
+  }
+));
+
+app.use('/', auth(passport));
+app.use('/', routes);
+
 // GET /: This route should only be accessible to logged in users.
 router.get('/', function(req, res, next) {
   // Your code here.
   res.render('index');
 });
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,6 +142,11 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = app;
