3160d9d8d274d847bb19f58cab37d372059194ac
Isaac Kapadia <ikapedia@umich.edu>
2016-06-25 10:47:56 -0400

    worked

--- day5/vault/app.js
+++ day5/vault/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var passport = require('passport');
 var app = express();
 
 // Express setup
@@ -14,6 +14,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Passport set-up
+var userPasswords = require('./passwords.hashed.json').passwords;
+var LocalStrategy = require('passport-local').Strategy;
+app.use(passport.initialize());
+app.use(passport.session());
+
+// cookies 
+// var cookieSession = require('cookie-session');
+// app.use(cookieSession({keys: ['secret'], maxAge:1000*60*2}));
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+app.use(session( {
+  secret: 'secret',
+store: new MongoStore({mongooseConnection: require('mongoose').connection})
+}));
+
+
 // This is the function we're going to use in Phases 4 and 5 to hash
 // user passwords.
 function hashPassword(password) {
@@ -22,17 +40,112 @@ function hashPassword(password) {
   return hash.digest('hex');
 }
 
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  // userPasswords.forEach(function(user) {
+  //   if (user._id === id) {
+  //     done(null, user);
+  //   }
+  // })f
+
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 // Models are defined in models/models.js
 var models = require('./models/models');
 
 // SET UP PASSPORT HERE
+passport.use(new LocalStrategy (
+  function(username, password, done) {
+    User.findOne({username: username}, function(err, user) {
+      if (user.password === password) {
+        done(null, user);
+      }
+      else {
+        done(null,false);
+      }
+    })
+    // var hashedPassword = hashPassword(password);
+    
+    // userPasswords.forEach(function(user) {
 
-// GET /: This route should only be accessible to logged in users.
-router.get('/', function(req, res, next) {
-  // Your code here.
-  res.render('index');
+    //   if (user.username === username && user.password === hashedPassword) {
+    //     u = user;
+    //   }
+    // });
+    // return done(null, u);
+
+
+    // var u = false;
+    // userPasswords.forEach(function(user) {
+
+    //   if (user.username === username && user.password === password) {
+    //     u = user;
+    //   }
+    // });
+    // return done(null, u);
+}));
+
+app.get('/signup', function(req, res, next) {
+    res.render('register');
+    });
+
+    app.post('/signup', function(req, res, next) {
+
+      var username = req.body.username;
+      var password = req.body.password;
+      
+
+      // can also make sure that fields aren't empty
+
+      // check if password fields are equivalent, then allow user to register and redirect to login page
+      
+        user = new User ({
+          username: username,
+          password: password
+        });
+
+        user.save(function(err) {
+          if (err) {
+            console.log('error registering');
+          }
+          else {
+            res.redirect('login');
+          }
+        }); 
+        
+    });
+
+
+app.get('/login', function(req, res) {
+    res.render('login');
 });
 
+app.post('/login', passport.authenticate('local', {
+  
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/login');
+});
+
+// GET /: This route should only be accessible to logged in users.
+app.get('/', function(req, res) {
+ if (!req.isAuthenticated()) {
+   res.render('index');
+ }
+ else {
+  res.redirect('/login');
+ }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- day5/vault/models/models.js
+++ day5/vault/models/models.js
@@ -1,9 +1,18 @@
 "use strict";
 
 var mongoose = require('mongoose');
-var connect = process.env.MONGODB_URI || require('./models/connect');
+var connect = process.env.MONGODB_URI || require('./connect');
 mongoose.connect(connect);
+var User = new mongoose.Schema ({
+	username: {
+	    type: String,
+	    required: true
+	 },
+	password: {
+		type: String,
+		required: true
+	}
+})
 
-module.exports = {
-  // YOUR MODELS HERE
-};
+
+module.exports = mongoose.model('User', User);
