15f55962de0affef87c09c54fcc686fdd42039b4
Villain1 <anirudhramesh1@gmail.com>
2016-06-25 11:36:11 -0400

    might not have finished last part
    might not have finished last part

--- day5/vault/app.js
+++ day5/vault/app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var userPasswords = require('./passwords.hashed.json').passwords;
+var session = require('express-session');
+var cookieSession = require('cookie-session');
+var MongoStore = require('connect-mongo')(session);
+var model = require('./models/models');
+var User = model.User;
+
 
 var app = express();
 
@@ -13,6 +22,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Passport Setup
+
+app.use(session({
+    secret: "secret",
+    store: new MongoStore({mongooseConnection: require('mongoose').connection})
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // This is the function we're going to use in Phases 4 and 5 to hash
 // user passwords.
@@ -27,10 +46,81 @@ var models = require('./models/models');
 
 // SET UP PASSPORT HERE
 
+passport.serializeUser(function(user, done) {
+  console.log("serialize");
+  console.log(user);
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user){
+    return done(null, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    User.findOne({username: username}, function(err, user){
+      if(user.password === password){
+        return done(null, user);
+      }else{
+        return done(null, false);
+      }
+    })
+  }
+));
+
+function check(req, res, next){
+  if(req.isAuthenticated()){
+    console.log("authenticated");
+    return next();
+  }else{
+    console.log("not authenticated");
+    res.redirect('/login');
+  }
+}
+
 // GET /: This route should only be accessible to logged in users.
-router.get('/', function(req, res, next) {
+
+app.get('/', function(req, res, next) {
+  console.log("checking");
   // Your code here.
   res.render('index');
+  // res.send('ok');
+});
+
+
+app.get('/login', function(req, res, next){
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+app.get('/logout', function(req, res, next){
+  req.logout();
+  res.redirect('/login');
+});
+
+app.get('/signup', function(req, res, next){
+  res.render('signup');
+});
+
+app.post('/signup', function(req, res, next){
+  console.log('got page');
+  var user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  user.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect('/');
+    }
+  })
 });
 
 
--- day5/vault/models/models.js
+++ day5/vault/models/models.js
@@ -1,9 +1,19 @@
 "use strict";
 
 var mongoose = require('mongoose');
-var connect = process.env.MONGODB_URI || require('./models/connect');
+var connect = process.env.MONGODB_URI || require('./connect');
 mongoose.connect(connect);
 
 module.exports = {
-  // YOUR MODELS HERE
+  User: mongoose.model('User', new mongoose.Schema({
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  }))
+
 };
