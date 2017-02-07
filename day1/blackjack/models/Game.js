0f90faee0b170fb665bcc944069dafbfd3fa2dae
Jonathan Zinger <jzinger1@babson.edu>
2017-02-06 20:49:21 -0800

    [added] Blackjack 1.0

--- day1/blackjack/models/Game.js
+++ day1/blackjack/models/Game.js
@@ -1,7 +1,24 @@
 var mongoose = require('mongoose');
 
 var GameSchema = new mongoose.Schema({
-  // YOUR CODE HERE
+  bet: Number,
+  playerCards: Array,
+  dealerCards: Array,
+  deck: Array,
+  playerTotal: {
+    type: Number,
+    default: 0
+  },
+  dealerTotal: {
+    type: Number,
+    default: 0
+  },
+  gameStatus: {
+    type: String,
+    default: "Not Started"
+  },
+  playerStatus: String,
+  dealerStatus: String
 });
 
 GameSchema.statics.newGame = function(item, callback){
@@ -11,38 +28,131 @@ GameSchema.statics.newGame = function(item, callback){
 }
 
 function Card(suit, val, symbol) {
-  // YOUR CODE HERE
+  this.suit = suit;
+  this.val = val;
+  this.symbol = symbol;
 }
 
 function Deck(){
   this.deck = [];
-  this.createDeck()
-  this.shuffleDeck()
+  this.createDeck();
+  this.shuffleDeck();
   return this.deck;
 }
 
 Deck.prototype.createDeck = function() {
   // YOUR CODE HERE
+  var suits = ['hearts','diamonds','clubs','spades'];
+  var vals = [11,2,3,4,5,6,7,8,9,10,10,10,10];
+  var symbols = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  for(var i = 0; i < suits.length; i++) {
+    for(var j = 0; j < vals.length; j++) {
+      var card = new Card(suits[i], vals[j], symbols[j]);
+      this.deck.push(card);
+    }
+  }
 }
 
 Deck.prototype.shuffleDeck = function() {
   // YOUR CODE HERE
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  var shuffledDeck = [];
+
+  while(this.deck.length) {
+    shuffledDeck.push(this.deck.splice(getRandomInt(0, this.deck.length), 1)[0])
+  }
+
+  this.deck = shuffledDeck;
 }
 
 GameSchema.methods.calcValue = function(hand){
   // YOUR CODE HERE
+  var total = 0;
+
+  hand.sort(function(a,b) {
+    return a.val-b.val;
+  })
+
+  for(var i = 0; i < hand.length; i++) {
+    if(hand[i].symbol === 'A' && total > 10) {
+      hand[i].val = 1;
+      total += hand[i].val;
+    } else {
+      total += hand[i].val;
+    }
+  }
+  return total;
 }
 
 GameSchema.methods.dealInitial = function() {
   // YOUR CODE HERE
+  this.gameStatus = 'In Progress';
+  this.playerCards.push(this.deck.shift());
+  this.dealerCards.push(this.deck.shift());
+  this.playerCards.push(this.deck.shift());
+  this.dealerCards.push(this.deck.shift());
+  this.playerTotal = this.calcValue(this.playerCards);
+  this.dealerTotal = this.calcValue(this.dealerCards);
+  if(this.playerTotal > this.dealerTotal && this.playerTotal === 21) {
+    this.playerStatus = 'Won';
+    this.dealerStatus = 'Lost';
+    this.gameOver();
+  } else if(this.dealerTotal < this.playerTotal && this.dealerTotal === 21) {
+    this.playerStatus = 'Lost';
+    this.dealerStatus = 'Won';
+    this.gameOver();
+  } else if(this.playerTotal === 21 && this.dealerTotal === 21) {
+    this.playerStatus = 'Tied';
+    this.dealerStatus = 'Tied';
+    this.gameOver();
+  } else if(this.playerTotal > this.dealerTotal) {
+    this.playerStatus = 'Winning';
+    this.dealerStatus = 'Losing';
+  } else if(this.playerTotal < this.dealerTotal) {
+    this.playerStatus = 'Losing';
+    this.dealerStatus = 'Winning';
+  } else if(this.playerTotal === this.dealerTotal) {
+    this.playerStatus = 'Tied';
+    this.dealerStatus = 'Tied';
+  }
 };
 
 GameSchema.methods.hit = function(){
   // YOUR CODE HERE
+  this.playerCards.push(this.deck.shift());
+  this.playerTotal = this.calcValue(this.playerCards);
+  if(this.playerTotal === 21) {
+    this.playerStatus = 'Won';
+    this.dealerStatus = 'Lost';
+    this.gameOver();
+  } else if(this.playerTotal > this.dealerTotal) {
+    this.playerStatus = 'Winning';
+    this.dealerStatus = 'Losing';
+  } else if(this.playerTotal < this.dealerTotal) {
+    this.playerStatus = 'Losing';
+    this.dealerStatus = 'Winning';
+  } else if(this.playerTotal === this.dealerTotal) {
+    this.playerStatus = 'Tied';
+    this.dealerStatus = 'Tied';
+  }
 };
 
 GameSchema.methods.stand = function(){
   // YOUR CODE HERE
+  while(this.dealerTotal < 17) {
+    this.dealerCards.push(this.deck.shift());
+    this.dealerTotal = this.calcValue(this.dealerCards);
+  }
+  this.dealerTotal = this.calcValue(this.dealerCards);
+  if(this.dealerTotal > 21) {
+    this.playerStatus = 'Won';
+    this.dealerStatus = 'Lost';
+  } else if(this.dealerTotal > this.playerTotal)
 }
 
 GameSchema.methods.gameOver = function(){
