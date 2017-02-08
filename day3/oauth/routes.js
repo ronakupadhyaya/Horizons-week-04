8475b7ef71c96903e5839be17765f43d194215f9
Snuupy <snuupy[-removethis-]@snusite.com>
2017-02-08 11:06:52 -0800

    git ignore and github oauth

--- day3/oauth/routes.js
+++ day3/oauth/routes.js
@@ -3,20 +3,54 @@ var router = express.Router();
 
 var passport = require('passport');
 
+var env = require("./env.js");
+
+
 // YOUR GITHUB STRATEGY HERE
+var GitHubStrategy = require('passport-github').Strategy;
+
+console.log(env.GITHUB_CLIENT_ID);
+console.log(env.GITHUB_CLIENT_SECRET);
+passport.use(new GitHubStrategy({
+    clientID: env.GITHUB_CLIENT_ID,
+    clientSecret: env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    cb(null, profile);
+  }
+));
+
 
 passport.serializeUser(function(user, done) {
   done(null, user.username);
 });
 passport.deserializeUser(function(username, done) {
-  done(null, {username: username});
+  done(null, {
+    username: username
+  });
 });
 router.use(passport.initialize());
 router.use(passport.session());
 
 // YOUR GET /auth/github ENDPOINT HERE
+router.get('/auth/github',
+  passport.authenticate('github'));
+
 
 // YOUR GET /auth/github/callback ENDPOINT HERE
+router.get('/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/login'
+  }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 router.get('/login', function(req, res) {
   res.render('login');
@@ -37,5 +71,7 @@ router.get('/', function(req, res) {
     res.redirect('/login');
   }
 });
+
+
 
 module.exports = router;
