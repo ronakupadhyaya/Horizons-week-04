cc048e52593816e6df358e98f5f8bdff271e43e1
Moose Paksoy <mustafa@joinhorizons.com>
2017-02-08 11:50:38 -0800

    Add solution to oauth.

--- day3/oauth/routes.js
+++ day3/oauth/routes.js
@@ -1,22 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
 
-// YOUR GITHUB STRATEGY HERE
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_SECRET,
+  scope: 'repo'
+},
+function(token, refreshToken, profile, done) {
+  console.log(profile);
+  done(null, {
+    token: token,
+    username: profile.username
+  });
+}));
 
 passport.serializeUser(function(user, done) {
-  done(null, user.username);
+  done(null, JSON.stringify(user));
 });
-passport.deserializeUser(function(username, done) {
-  done(null, {username: username});
+
+passport.deserializeUser(function(userJson, done) {
+  done(null, JSON.parse(userJson));
 });
+
 router.use(passport.initialize());
 router.use(passport.session());
 
-// YOUR GET /auth/github ENDPOINT HERE
-
-// YOUR GET /auth/github/callback ENDPOINT HERE
+router.get('/auth/github', passport.authenticate('github'));
+router.get('/auth/github/callback', passport.authenticate('github', {
+  failureRedirect: '/login'
+}),
+  function(req, res) {
+    console.log('Successfully logged in to GitHub');
+    res.redirect('/');
+});
 
 router.get('/login', function(req, res) {
   res.render('login');
@@ -36,6 +56,26 @@ router.get('/', function(req, res) {
   } else {
     res.redirect('/login');
   }
+});
+
+router.get('/repos', function(req, res) {
+  request('https://api.github.com/user/repos', {
+    headers: {
+      'User-Agent': 'request'
+    },
+    qs: {
+      access_token: req.user.token
+    },
+    json: true
+  }, function(err, response, body) {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.render('repos', {
+        repos: body
+      });
+    }
+  });
 });
 
 module.exports = router;
