f2a0913a8b8d7e722179bc90d6c6bba6affe7351
Snuupy <snuupy[-removethis-]@snusite.com>
2017-02-08 10:03:47 -0800

    routes

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next) {
+	if (!req.user) {
+		res.send("no");
+	}
+	return;
+});
+
 router.get('/', function(req, res) {
-  res.render('index', {
-    user: req.user
-  });
+	res.render('index', {
+		user: req.user
+	});
 });
 
 router.get('/private', function(req, res) {
-  res.render('index', {
-    user: req.user
-  });
+	res.render('index', {
+		user: req.user
+	});
 });
 
 router.get('/secret', function(req, res) {
-  res.render('index', {
-    user: req.user
-  });
+	res.render('index', {
+		user: req.user
+	});
 });
 
 module.exports = router;
