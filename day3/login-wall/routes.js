f7dc6bf924fdc9d2dc0c3690224cd7ed8758b662
Moose Paksoy <mustafa@joinhorizons.com>
2017-02-08 11:08:36 -0800

    Add solution to login-wall

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    console.log('REDIRECT TO /login');
+    res.redirect('/login');
+  }
+});
+
 router.get('/', function(req, res) {
-  res.render('index', {
-    user: req.user
-  });
+  if (req.user) {
+    res.render('index', {
+      user: req.user
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/private', function(req, res) {
