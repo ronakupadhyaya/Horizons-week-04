b2f036b657a2cce3e38eae1b2a52e1d7069ac651
Daniel <danielkwak7@gmail.com>
2017-02-09 09:23:56 -0800

    stuff

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next) {
+  if (req.user) {
+    console.log("woeifjoweifjoejwf");
+    next();
+  } else {
+    res.redirect('/login');
+console.log("woefjweoifj");
+
+  }
+})
+
 router.get('/', function(req, res) {
   res.render('index', {
     user: req.user
--- day3/oauth/routes.js
+++ day3/oauth/routes.js
@@ -4,6 +4,22 @@ var router = express.Router();
 var passport = require('passport');
 
 // YOUR GITHUB STRATEGY HERE
+var GitHubStrategy = require('passport-github').Strategy;
+
+passport.use(new GitHubStrategy({
+    clientID: "c0bee982c41669f90a9e",
+    clientSecret: "b35a92c209ae66e97b90d5738fa3d395c374e135",
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    cb(null, profile);
+    //JUST LIEK WRITING DONE()
+    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
+
 
 passport.serializeUser(function(user, done) {
   done(null, user.username);
@@ -15,8 +31,19 @@ router.use(passport.initialize());
 router.use(passport.session());
 
 // YOUR GET /auth/github ENDPOINT HERE
+router.get('/auth/github',
+  passport.authenticate('github'));
+
+
 
 // YOUR GET /auth/github/callback ENDPOINT HERE
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 router.get('/login', function(req, res) {
   res.render('login');
@@ -37,5 +64,17 @@ router.get('/', function(req, res) {
     res.redirect('/login');
   }
 });
+
+
+//get repos
+
+router.get('/repos', function(req, res) {
+  request('https://api.github.com/user/repos', {
+    headers: {
+      'User-Agent': "request"
+    },
+    
+  })
+})
 
 module.exports = router;
