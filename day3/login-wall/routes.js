1084166219a2c49b8217673b00ca06e6650638b1
Eric Abenojar <abenoeric@gmail.com>
2017-02-09 09:23:29 -0800

    Day3

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index', {
     user: req.user
   });
+  next();
 });
 
 router.get('/private', function(req, res) {
