b0151d06fa6c74a29267795f476823084e69e66a
git push -u origin <mtroyanovsky1@babson.edu>
2017-02-10 14:18:17 -0800

    love

--- day3/oauth/routes.js
+++ day3/oauth/routes.js
@@ -4,17 +4,51 @@ var router = express.Router();
 var passport = require('passport');
 
 // YOUR GITHUB STRATEGY HERE
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(token, refreshToken, profile, done) {
+      done(null, {
+        token: token,
+        username:profile.username
+      });
+  }
+));
 
 passport.serializeUser(function(user, done) {
   done(null, user.username);
 });
-passport.deserializeUser(function(username, done) {
-  done(null, {username: username});
+passport.deserializeUser(function(userJson, done) {
+  done(null, Json.parse(userJson));
 });
 router.use(passport.initialize());
 router.use(passport.session());
 
 // YOUR GET /auth/github ENDPOINT HERE
+var GitHubStrategy = require('passport-github').Strategy;
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      cb(null, profile);
+  }
+));
+
+router.get('/auth/github',
+  passport.authenticate('github'));
+
+router.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 // YOUR GET /auth/github/callback ENDPOINT HERE
 
@@ -37,5 +71,6 @@ router.get('/', function(req, res) {
     res.redirect('/login');
   }
 });
+
 
 module.exports = router;
