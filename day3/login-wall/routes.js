b26b493740b90b0b06a46a1992813ffb3eeb2b66
clairehuang1 <huangcl@sas.upenn.edu>
2017-02-10 14:00:28 -0800

    moar udpates

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', function(req,res,next){
+  if (!req.user){
+    res.redirect('/login');
+  }else{
+    res.redirect('/');
+  }
+  next();
+})
+
 router.get('/', function(req, res) {
   res.render('index', {
     user: req.user
