46c48a450a620330b5552d097db241f9b535bd2f
Mustafa Paksoy <mustafa@joinhorizons.com>
2017-02-10 12:43:38 -0800

    Update exercise2.js

--- day5/hexpress/exercise2.js
+++ day5/hexpress/exercise2.js
@@ -1,20 +1,31 @@
 var hexpress = require('./hexpress');
 var app = hexpress();
 
-// This route will respond with status 200 and the
-// JSON object {success: true, username: req.body.username, password: req.body.password}
-// where req.body.username will be the username posted to the endpoint
-// and req.body.password will be the password posted to the endpoint
-//
-// Use POSTMAN to POST to localhost:3000/login with body
+// Use POSTMAN to make a POST request to localhost:3000/login with body
 //  - username: your-username
 //  - password: your-password
+//
+// This route will respond with status 200 and the
+// JSON object {method: 'post', username: req.body.username, password: req.body.password}
+// where req.body.username will be the username posted to the endpoint
+// and req.body.password will be the password posted to the endpoint
 app.post('/login', function(req, res) {
   res.json({n
-    success: true,
+    method: 'post',
     username: req.body.username,
     password: req.body.password
   });
 });
 
+// Use POSTMAN to make a GET request to localhost:3000/login?username=moose&password=prath
+//
+// This route will respond with status 200 and the
+// JSON object {method: true, username: 'moose', password: 'prath'}
+app.get('/login', function(req, res) {
+  res.json({n
+    success: true,
+    username: req.query.username,
+    password: req.query.password
+  });
+  
 app.listen(3000);
