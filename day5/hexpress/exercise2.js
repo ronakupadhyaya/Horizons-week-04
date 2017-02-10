4806dc8e48d7566126b1758ad1c85f9a7bd8599a
Prathmesh Desai <d.prathmesh@gmail.com>
2017-02-10 15:03:46 -0800

    Update exercise2.js

--- day5/hexpress/exercise2.js
+++ day5/hexpress/exercise2.js
@@ -10,7 +10,7 @@ var app = hexpress();
 // where req.body.username will be the username posted to the endpoint
 // and req.body.password will be the password posted to the endpoint
 app.post('/login', function(req, res) {
-  res.json({n
+  res.json({
     method: 'post',
     username: req.body.username,
     password: req.body.password
@@ -22,7 +22,7 @@ app.post('/login', function(req, res) {
 // This route will respond with status 200 and the
 // JSON object {method: true, username: 'moose', password: 'prath'}
 app.get('/login', function(req, res) {
-  res.json({n
+  res.json({
     success: true,
     username: req.query.username,
     password: req.query.password
