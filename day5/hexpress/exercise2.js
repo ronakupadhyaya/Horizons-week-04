ae45afe60a21cf059261b5684cdfe34b48701cbc
Irvin <irvink.tang@gmail.com>
2017-02-13 15:34:46 -0800

    week4

--- day5/hexpress/exercise2.js
+++ day5/hexpress/exercise2.js
@@ -10,7 +10,7 @@ var app = hexpress();
 // where req.body.username will be the username posted to the endpoint
 // and req.body.password will be the password posted to the endpoint
 app.post('/login', function(req, res) {
-  res.json({n
+  res.json({
     method: 'post',
     username: req.body.username,
     password: req.body.password
@@ -22,10 +22,11 @@ app.post('/login', function(req, res) {
 // This route will respond with status 200 and the
 // JSON object {method: true, username: 'moose', password: 'prath'}
 app.get('/login', function(req, res) {
-  res.json({n
+  res.json({
     success: true,
     username: req.query.username,
     password: req.query.password
   });
-  
+});
+
 app.listen(3000);
--- day5/hexpress/hexpress.js
+++ day5/hexpress/hexpress.js
@@ -1,10 +1,99 @@
-module.exports = function () {
-  // YOUR CODE HERE
+var http = require('http');
+var querystring = require('querystring');
 
-  return {
-    get: function(route, callback) {
-      // YOUR GET METHOD HERE
+var checkArray = function(routePrefix,route){
+  console.log(routePrefix)
+  var rPre = routePrefix.split('/');
+  var rSpl = route.split('/');
+  if(routePrefix === '/') {
+    return true;
+  } else {
+    for(var i=0;i<rPre.length;i++) {
+      if(rPre[i] !== rSpl[i]){
+        return false;
+      }
+    }} return true;
+  }
+
+  module.exports = function () {
+    var routes = [];
+
+    return {
+      get: function(route, callback) {
+        routes.push({
+          route:route,
+          callback:callback,
+          method:'GET'
+        })
+      },
+      post: function(route, callback) {
+        routes.push({
+          route:route,
+          callback:callback,
+          method:'POST'
+        })
+      },
+      use: function(routePrefix, callback) {
+        if(typeof routePrefix === 'string'){
+          routes.push({
+            route: routePrefix,
+            callback:callback,
+            method:'USE'
+          })
+        } else{
+          routes.push({
+            route:'/',
+            callback:routePrefix,
+            method:'USE'
+          })
+        }
+      },
+      listen: function(port) {
+        var server = http.createServer(function(req,res){
+          res.send = function(data) {
+            this.writeHead(200, {'Content-Type': 'text/plain'})
+            this.end(data);
+          };
+          res.json = function(data) {
+            this.setHeader('Content-Type', 'application/json')
+            this.end(JSON.stringify(data))
+          };
+          res.render = function(name,options){
+
+          }
+
+          var url = req.url.split('?');
+          req.query = querystring.parse(url[1]);
+          console.log('my query is', req.query);
+
+          for(var i=0;i<routes.length;i++){
+            var route = routes[i];
+            if(route.route === url[0] && req.method === route.method){
+              if(route.method === 'GET') {
+                route.callback(req,res);
+                return;
+              } else if (route.method === 'POST') {
+                var body = '';
+                req.on('readable', function() {
+                  var chunk = req.read();
+                  if (chunk) body += chunk;
+                });
+                req.on('end', function() {
+                  // queryString is the querystring node built-in
+                  req.body = querystring.parse(body);
+                  route.callback(req,res);
+                });
+                return;
+              }
+            }
+            else if(checkArray(route.route,url[0]) && route.method === 'USE') {
+              route.callback(req,res);
+              return;
+            }
+            //todo if no routes matches display error
+          }
+        });
+        server.listen(port)
+      }
     }
-    // YOUR METHODS HERE
-  };
-};
+  }
