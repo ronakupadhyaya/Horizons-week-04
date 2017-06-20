6816cb21cf285dbf0850f12d7f98943437535a28
sukwhan (david) <younsa@bc.edu>
2017-06-20 09:30:07 -0700

    removed git repo inception

--- day1/table/public/toggle.js
+++
@@ -1,6 +0,0 @@
-$('.table').on('click', '.btn', function(evt) {
-  console.log(evt);
-  $(this).toggleClass('btn-warning')
-  $(this).toggleClass('btn-success')
-  $(this).text() === 'todo' ? $(this).text('done') : $(this).text('todo')
-})
