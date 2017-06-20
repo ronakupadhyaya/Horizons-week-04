04dd559c67499d2dee46aca90a205e2e73a990ad
maxwell3739 <maxwell@uvic.ca>
2017-06-20 09:09:21 -0700

    finish week01

--- day1/blackjack/models/Game.js
+++ day1/blackjack/models/Game.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 
 var GameSchema = new mongoose.Schema({
-  // YOUR CODE HERE
+  bet: Number,
+  playerCards: Array,
+  dealerCards: Array,
+  deckCards: Array,
+  playerVal: Number,
+  dealerVal: Number,
+  gameStatus: String,
+  playerStatus: String,
+  dealerStatus: String
 });
 
 GameSchema.statics.newGame = function(item, callback){
@@ -11,7 +19,9 @@ GameSchema.statics.newGame = function(item, callback){
 }
 
 function Card(suit, val, symbol) {
-  // YOUR CODE HERE
+  this.suit = suit
+  this.val = val
+  this.symbol = symbol
 }
 
 function Deck(){
@@ -21,32 +31,130 @@ function Deck(){
   return this.deck;
 }
 
+// 1. Create Deck
 Deck.prototype.createDeck = function() {
-  // YOUR CODE HERE
+  var deck = []
+  var suit = ["hearts", "spades", "diamonds", "clubs"]
+  var value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+  var symbol = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+//LOOP THROUGH SUITS
+  for (j=0; j<suit.length; j++) {
+//FOR LOOP FOR VALUE SYMBOL
+  for (i=0; i<value.length; i++) {
+      var card = new Card(suit[j], value[i], symbol[i])
+      deck.push(card)
+      }
+  }
+  return deck
 }
 
+// 2. Shuffle Deck
 Deck.prototype.shuffleDeck = function() {
-  // YOUR CODE HERE
-}
+  //NUMBERS WILL BE ARRAY OF 52 NUMBERS IN RANDOM ORDER
+  var numbers = []
+  var arrRandom = []
+  var deckRandom = []
+  var leftToPick = 52
+for (i=0; i<52; i++) {
+  numbers.push(i) }
+  var pick = function() {
+    //CHECK IF ALL ARE PICKED
+    if(leftToPick === 0) {
+      return }
+      //IF NOT GRAB A RANDOM INDEX
+    var randomIndex = Math.floor(Math.random() * leftToPick)
+    //PUT A NUMBER IN WITH THAT INDEX
+    arrRandom.push(numbers[randomIndex])
+    //TAKE THAT OUT OF THE NUMBERS ARRAY
+    numbers.splice(randomIndex,1)
+    leftToPick--
+    //DO AGAIN
+    pick()
+  }
+  pick()
+  var newDeck = Deck.prototype.createDeck()
+    for (j=0; j<52; j++) {
+      deckRandom.push(newDeck[arrRandom[j]])
+    }
+    return deckRandom
+  }
 
 GameSchema.methods.calcValue = function(hand){
-  // YOUR CODE HERE
+  var totalNonAce = 0
+  var totalAce = 0
+  hand.forEach(function(card) {
+    if (card.val === 11) {
+      totalAce += card.val
+    }
+    else {
+      totalNonAce += card.val
+    }
+  })
+  var total = totalNonAce + totalAce
+  var reduceAces = function() {
+    console.log(totalAce)
+    console.log(total)
+    console.log(totalNonAce)
+    if ((total>21) && (totalAce>0)) {
+      totalAce -= 10
+      total = totalNonAce + totalAce
+      reduceAces()
+    }
+  }
+reduceAces()
+
+return total
+
 }
 
 GameSchema.methods.dealInitial = function() {
-  // YOUR CODE HERE
+  this.playerCards = this.deckCards.splice(0,2)
+  this.dealerCards = this.deckCards.splice(0,2)
+  this.playerVal = GameSchema.methods.calcValue(this.playerCards)
+  this.dealerVal = GameSchema.methods.calcValue(this.dealerCards)
+  this.gameStatus = "In Progress"
+  this.playerStatus = "In Progress"
+  this.dealerStatus = "In Progress"
 };
 
 GameSchema.methods.hit = function(){
-  // YOUR CODE HERE
+  this.playerCards.push(this.deckCards.splice(0,1))
+  this.playerVal = GameSchema.methods.calcValue(this.playerCards)
+  if (this.playerVal>21) {
+    gameOver()
+  }
 };
 
 GameSchema.methods.stand = function(){
-  // YOUR CODE HERE
+
+var hitDealer = function() {
+  if(this.dealerVal>16) {
+    this.dealerCards.push(this.deckCards.splice(0,1))
+    hitDealer()
+    }
+  }
+  hitDealer()
+  gameOver()
 }
 
 GameSchema.methods.gameOver = function(){
-  // YOUR CODE HERE
+  this.gameStatus = "Over"
+
+  if(this.dealerVal>this.playerVal) {
+    this.playerStatus = "Lost"
+    this.dealerStatus = "Won" }
+
+  if(this.playerVal>this.dealerVal) {
+    this.playerStatus = "Won"
+    this.dealerStatus = "Lost" }
+
+  if(this.playerVal>21) {
+    this.playerStatus = "Lost"
+    this.dealerStatus = "Won" }
+
+  if(this.dealerVal>21) {
+    this.playerStatus = "Won"
+    this.dealerStatus = "Lost" }
 }
 
 module.exports = mongoose.model('Game', GameSchema);
--- day1/blackjack/routes/index.js
+++ day1/blackjack/routes/index.js
@@ -4,20 +4,53 @@ var router = express.Router({ mergeParams: true });
 var GameModel = require('../models/Game.js');
 
 var gameRepresentation = function(game) {
-  // YOUR CODE HERE
+return {
+  id: game._id,
+  playerBet: game.playerBet,
+  status: game.status,
+  userTotal : game.userTotal,
+  dealerTotal : game.dealerTotal,
+  userStatus : game.userStatus,
+  dealerStatus : game.dealerStatus,
+  currentPlayerHand : game.currentPlayerHand,
+  houseHand : game.houseHand
+  }
 }
 
 router.get('/', function (req, res, next) {
-  // YOUR CODE HERE
+  GameModel.find(function(err, games) {
+    if (err) return res.status(500).send(err);
+    if (req.query.status) {
+      games = games.filter(function(game) {
+        if (req.query.status === game.status) {
+          return game;
+        }
+      });
+    }
+    res.render('index', {
+      games: games.map(function(game) {
+        return { id: game._id, status: game.status }
+      })
+    })
+  })
 });
 
 router.post('/game', function(req, res, next) {
-  // YOUR CODE HERE
+  GameModel.newGame({}, function(err,game) {
+    if(err) return res.status(500).send(err);
+    res.redirect('/game/' + game._id)
+  })
 });
 
 router.get('/game/:id', function(req, res, next) {
-  // YOUR CODE HERE
-});
+  GameModel.findById(req.params.id, function(err, game) {
+    if(err) return res.status(500).send(err);
+    res.render('viewgame', {
+        title: "View GameModel"
+        game: gameRepresentation(game)
+      })
+    })
+  })
 
 router.post('/game/:id', function(req, res, next) {
   // YOUR CODE HERE
