437f8ef1ed4c416d182fd7cd0cf1a46dd17bc43c
allaunyc <alau11@binghamton.edu>
2017-06-20 16:48:17 -0700

    day2 yelp

---
+++ day2/examples/part1/env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI=mongodb://allaunyc:8z25lwe8@ds137141.mlab.com:37141/advancedpart1;
---
+++ day2/examples/part2/env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI=mongodb://allaunyc:8z25lwe8@ds133192.mlab.com:33192/advancedpart2;
