917ce403302ceaed4e08d532d4ae52bb4c4c1528
JingyW <wangj2@whitman.edu>
2017-06-21 11:22:51 -0700

    login wall

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,19 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+function isLoggedIn(req,res,next) {
+  if (req.user){
+    next();
+  } else {
+    res.redirect('/login')
+  }
+}
+
+/*
+or it can be
+router.use(function(req,res,next){
+  if (req.user) {
+  ...
+}
+})
+this equals to:
+router.use('/',function(...))
+this middleware has to be above everything else
+*/
+
+router.get('/', isLoggedIn,function(req, res) {
   res.render('index', {
     user: req.user
   });
 });
 
-router.get('/private', function(req, res) {
+router.get('/private', isLoggedIn,function(req, res) {
   res.render('index', {
     user: req.user
   });
 });
 
-router.get('/secret', function(req, res) {
+router.get('/secret', isLoggedIn,function(req, res) {
   res.render('index', {
     user: req.user
   });
