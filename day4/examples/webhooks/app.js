7834905b7208c94ada742ddc4aeaf23fae4db8d2
JingyW <wangj2@whitman.edu>
2017-06-22 11:28:58 -0700

    github

--- day4/examples/webhooks/app.js
+++ day4/examples/webhooks/app.js
@@ -7,7 +7,8 @@ var exphbs  = require('express-handlebars');
 //setup mongoose connection
 mongoose.connection.on('error', function(){
   console.log('error connecting to database')
-}
+})
+
 mongoose.connection.on('connected', function(){
   console.log('succesfully connected to database')
 })
@@ -20,8 +21,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 //ROUTES GO HERE
-
+app.post('/handletext',function(req,res) {
+  client.messages.create({
+  to: "+15099644566",
+  from: "+15092608221",
+  body: req.body.Body,
+  },function(err,message){
+    if (!err) {res.end()};
+  })
+})
 //add a route that will respond to post requests sent by Twilio via
 //webhooks
 
---
+++ day4/examples/webhooks/env.sh
@@ -0,0 +1,3 @@
+export MONGODB_URI="mongodb://Jingyw:webhook@ds135552.mlab.com:35552/webhook_twilio"
+export TWILIO_SID="AC0ccb3d26d83d1e236024c9b70d9efcfc"
+export TWILIO_AUTH_TOKEN="a69aab99e9834be73ed508e1ac1cecb6"
