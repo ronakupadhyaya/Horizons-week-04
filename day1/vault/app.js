b3eb673ea88ce423d92a47db71c14526bc7b60bf
Nate <n_straus3@outlook.com>
2017-06-21 17:24:25 -0700

    completed day1 up to part 4

--- day1/vault/app.js
+++ day1/vault/app.js
@@ -4,6 +4,12 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var crypto = require('crypto');
 
 // Express setup
 var app = express();
@@ -15,15 +21,81 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MONGODB SETUP HERE
+mongoose.connection.on('connected', function() {
+    console.log('Connected to MongoDb!');
+});
+mongoose.connect(process.env.MONGODB_URI);
 
 // SESSION SETUP HERE
+app.use(session({
+    secret: 'THEBESTENCODING',
+    store: new MongoStore({mongooseConnection: require('mongoose').connection})
+}));
 
 // PASSPORT LOCALSTRATEGY HERE
+passport.use(new LocalStrategy(function (username, password, done) {
+    var user = false;
+    var data = require("./passwords.hashed.json")
+    data.passwords.forEach(function (element) {
+        if (element.username === username &&
+            element.password === hashPassword(password)) {
+            user = element;
+        }
+    });
+    done(null, user);
+}));
 
 // PASSPORT SERIALIZE/DESERIALIZE USER HERE HERE
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    var user = false;
+    var data = require('./passwords.hashed.json');
+    data.passwords.forEach(function (element) {
+        if (element._id === id) {
+            user = element;
+        }
+    })
+    done(null, user);
+})
 
 // PASSPORT MIDDLEWARE HERE
+app.use(passport.initialize());
+app.use(passport.session());
 
 // YOUR ROUTES HERE
+app.get('/', function (req, res) {
+    if (!req.user) {
+        res.redirect('/login');
+    } else {
+        console.log(req.user);
+    res.render('index', {
+        user: req.user
+    });
+    }
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+})
+);
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function hashPassword(password) {
+    var hash = crypto.createHash('sha256');
+    hash.update(password);
+    return hash.digest('hex');
+}
 
 module.exports = app;
---
+++ day1/vault/env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI="mongodb://nstraus:1313@ds143608.mlab.com:43608/nstraus-vault"
