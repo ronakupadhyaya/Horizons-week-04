3374acb8fbe446f9cbdd67ede88415411668dbd7
Evan <Evanp1324@gmail.com>
2017-06-22 10:24:33 -0700

    day3 final commmit

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+// THE WALL!!! - any routes below this are protected!
+router.use(function(req, res, next){
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    return next();
+  }
+});
+
 router.get('/', function(req, res) {
   res.render('index', {
     user: req.user
