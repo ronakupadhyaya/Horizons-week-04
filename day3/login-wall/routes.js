fbdb7214643767e08f6c182116f73b13b4fbdb08
Mason <lantay@alumni.unc.edu>
2017-06-22 09:15:22 -0700

    day 3

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+///this is our code
+router.use(function(req, res, next){
+  if(req.user){
+    next();
+  } else{
+    res.redirect('login');
+  }
+})
+//that order matters
+//could also put it inside of passport after login
+
+//
 router.get('/', function(req, res) {
   res.render('index', {
     user: req.user
