e9f978b3b6e087362832b09b7f2532f7f520550d
Nicholas Park <nickpark1997@gmail.com>
2017-06-22 09:08:38 -0700

    commit

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//EVERYTHING BELOW "THE WALL"
+router.use(function(req, res, next){
+  if (!req.user) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+});
+
 router.get('/', function(req, res) {
   res.render('index', {
     user: req.user
