1648472d42e13859800151e679906b678a5b11db
git status <spike.lu@emory.edu>
2017-06-22 09:12:49 -0700

    hi

---
+++ day3/env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI='mongodb://test:1234@ds127962.mlab.com:27962/horizons-starter'
--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', function(req, res, next){
+  if(req.user) {
+    next();
+  } else {
+    res.send('You are not logged in. You should not be able to view this page!')
+  }
+})
+
 router.get('/', function(req, res) {
   res.render('index', {
     user: req.user
