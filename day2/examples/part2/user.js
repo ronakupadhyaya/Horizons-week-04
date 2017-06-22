e292e6da87fba98328f765e8d43c3607ac11370a
Ronak Upadhyaya <rdupadhy@usc.edu>
2017-06-22 09:31:20 -0700

    m

--- day2/examples/part2/user.js
+++ day2/examples/part2/user.js
@@ -16,6 +16,30 @@ var userSchema = new Schema({
   }
 });
 
+function getAge(birthday) {
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
+var age = userSchema.virtual('age');
+age.get(function(){
+  return getAge(this.birthday);
+});
+
+userSchema.methods.toggleGender = function(){
+  if(this.gender === 'male'){
+    this.gender = 'female';
+  }
+  else{
+    this.gender = 'male';
+  }
+}
+
+userSchema.statics.findByName = function(name, callback){
+  this.find({"name.first" : name,}, callback);
+}
+
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
