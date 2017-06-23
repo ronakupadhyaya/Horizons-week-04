94f0740be3d64f8cfa30f49b0d1aa60516105dc5
kritpetra <krit.petra@gmail.com>
2017-06-23 13:31:17 -0700

    Finished day 4

--- day4/examples/webhooks/app.js
+++ day4/examples/webhooks/app.js
@@ -3,11 +3,12 @@ var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var exphbs  = require('express-handlebars');
+var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 //setup mongoose connection
 mongoose.connection.on('error', function(){
   console.log('error connecting to database')
-}
+})
 mongoose.connection.on('connected', function(){
   console.log('succesfully connected to database')
 })
@@ -21,7 +22,14 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //ROUTES GO HERE
-
+app.post('/handletext', function(req, res) {
+  client.messages.create({
+    to: req.body.From,
+    from: req.body.To,
+    body: "Yo wazzup"
+  });
+  res.end();
+});
 //add a route that will respond to post requests sent by Twilio via
 //webhooks
 
