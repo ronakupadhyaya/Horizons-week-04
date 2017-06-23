a2f6d62be46fa0ca50b7d75b09d329b4c264a961
alexthkim <atk3@rice.edu>
2017-06-23 13:31:29 -0700

    double message

---
+++ day4/double-message/env.sh
@@ -0,0 +1,5 @@
+export MONGODB_URI=mongodb://alex:alex@ds135532.mlab.com:35532/akdoublemessage
+export SECRET=scrambledeggs
+export TWILIO_SID=AC73a6352b733379ec398e6020e0f52e38
+export TWILIO_AUTH_TOKEN=5024ef29914b47b3e18dceb1d62f06b6
+export MY_TWILIO_NUMBER="+14159430916"
