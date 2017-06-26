77d494cb312279027632a477fa920bb6b90378a9
sukwhan (david) <younsa@bc.edu>
2017-06-26 11:50:44 -0700

    week4

---
+++ day4/examples/webhooks/env.sh
@@ -0,0 +1,3 @@
+export MONGODB_URI="mongodb://syoun:youn0620@ds133922.mlab.com:33922/twilio-horizons"
+export TWILIO_SID="AC75c095eb73bf50211c32c8f1dd9d555d"
+export TWILIO_AUTH_TOKEN="618989391ac2bc81c1642db4642f0c7e"
