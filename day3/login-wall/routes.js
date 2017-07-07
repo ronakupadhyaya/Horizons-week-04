f88a1aaa8f748f37042bfb94414a208cd0c0a87f
Amy Wang <horselurrver@gmail.com>
2017-07-07 10:13:24 -0700

    Tried to add average rating functionality

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.use(function(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+});
+
+router.get('/', function(req, res, next) {
   res.render('index', {
     user: req.user
   });
