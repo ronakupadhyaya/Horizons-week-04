75f1616d502a38e58991c3982f49f58fbc0c3f46
natedana <natepdana@gmail.com>
2017-09-25 16:49:50 -0700

    w4d1

--- day1/blackjack/models/Game.js
+++ day1/blackjack/models/Game.js
@@ -1,52 +1,111 @@
 var mongoose = require('mongoose');
 
 var GameSchema = new mongoose.Schema({
-  // YOUR CODE HERE
+  bet: Number,
+  hand: Array,
+  dealer: Array,
+  deck: Array,
+  handval: {
+    type: Number,
+    default: 0
+  },
+  dealerval: {
+    type: Number,
+    default: 0
+  },
+  gamestatus: {
+    type: String,
+    default: "Not Started"
+  },
+  handstatus: String,
+  dealerstatus: String
 });
 
-GameSchema.statics.newGame = function(item, callback){
+GameSchema.statics.newGame = function(item, callback) {
   var game = new this(item)
   game.deck = new Deck();
   game.save(callback);
 }
 
 function Card(suit, val, symbol) {
-  // YOUR CODE HERE
+  val: Number,
+  suit: String,
+  symbol: String
 }
 
-function Deck(){
+function Deck() {
   this.deck = [];
-  this.createDeck()
-  this.shuffleDeck()
+  this.createDeck().bind(this)
+  this.shuffleDeck().bind(this)
   return this.deck;
 }
 
 Deck.prototype.createDeck = function() {
-  // YOUR CODE HERE
+  var suits = ['hearts', 'clubs', 'diamonds', 'spades'];
+  var symbols = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  var vals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
+  for (var i = 0; i < suits.length; i++) {
+    for (var j = 0; i < symbols.length; i++) {
+      this.deck.push(new Card(suits[i], vals[j], symbols[j]))
+    }
+  }
 }
 
 Deck.prototype.shuffleDeck = function() {
-  // YOUR CODE HERE
+  this.sort(function(a, b) {
+    return Math.random - 0.5
+  })
 }
 
-GameSchema.methods.calcValue = function(hand){
-  // YOUR CODE HERE
+GameSchema.methods.calcValue = function(hand) {
+  var total = 0;
+  var hasAce = false;
+  hand.forEach(function(card) {
+    total += card.val
+    if (card.symbol === "A") {
+      hasAce = true;
+    }
+  })
+  if (total > 21 && hasAce === true) {
+    total -= 10
+  }
+  return total
 }
 
 GameSchema.methods.dealInitial = function() {
-  // YOUR CODE HERE
+  hand.concat(this.deck.splice(0, 2));
+  dealer.concat(this.deck.splice(0, 2));
+  this.gamestatus = 'Started';
+  this.handstatus = 'In progress';
+  this.dealerstatus = 'In progress';
+  this.handval = this.calcValue(this.hand);
+  this.dealerval = this.calcValue(this.dealer);
 };
 
-GameSchema.methods.hit = function(){
-  // YOUR CODE HERE
+GameSchema.methods.hit = function() {
+  this.hand.concat(this.deck.splice(0, 1));
+  this.handval = this.calcValue(this.hand);
+  if (this.handval > 21) {
+    gameOver();
+  }
 };
 
-GameSchema.methods.stand = function(){
-  // YOUR CODE HERE
+GameSchema.methods.stand = function() {
+  while (this.dealerval < 17) {
+    this.dealer.concat(this.deck.splice(0, 1));
+    this.dealerval = this.calcValue(this.dealer);
+  }
+  gameOver();
 }
 
-GameSchema.methods.gameOver = function(){
-  // YOUR CODE HERE
+GameSchema.methods.gameOver = function() {
+  this.status = "Over";
+  this.handstatus = this.handval > 21 ? "Bust" : "Stand";
+  this.dealerstatus = this.dealerval > 21 ? "Bust" : "Stand";
+  if (this.handstatus === this.dealerstatus === "Stand") {
+    this.handstatus = this.hanval > this.dealerval ? "Win" : "Lose";
+    this.dealerstatus = this.hanval < this.dealerval ? "Win" : "Lose";
+  }
 }
 
 module.exports = mongoose.model('Game', GameSchema);
--- day1/vault/app.js
+++ day1/vault/app.js
@@ -4,26 +4,126 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var bank = require('./passwords.hashed.json');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
+var crypto = require('crypto');
+var models = require('./models/models');
 
-// Express setup
+
+// Express setup and middleware
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+function hashPassword(password) {
+  var hash = crypto.createHash('sha256');
+  hash.update(password)
+  return hash.digest('hex')
+}
+
 // MONGODB SETUP HERE
-
+mongoose.connect('mongodb://9724.mlab.com:49724/practicevault');
+mongoose.connection.on('connected', function() {
+  console.log('CONNECTED TO MONGODB');
+})
 // SESSION SETUP HERE
-
+app.use(session({
+  secret: 'nibbit',
+  store: new MongoStore({
+    mongooseConnection: require('mongoose').connection
+  })
+}));
 // PASSPORT LOCALSTRATEGY HERE
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    var hashedPW = hashPassword(password);
+    // var output = bank.passwords.find(function(element) {
+    //   return (element.username === username && element.password === hashedPW) ? true : false
+    // })
+    models.User.findOne({
+      username: username
+    }, function(err, result) {
+      if (result.password === hashedPW) {
+        done(null, result);
+      } else {
+        done(null, false);
+      }
+    })
+    // console.log(output);
+    // if (output) {
+    //   done(null, output)
+    // } else {
+    //   done(null, false);
+    // }
+  }
+));
 
 // PASSPORT SERIALIZE/DESERIALIZE USER HERE HERE
-
+passport.serializeUser(function(user, done) {
+  done(null, user._id)
+});
+passport.deserializeUser(function(id, done) {
+  // var user = bank.passwords.find(function(ele) {
+  //   return ele._id === id ? true : false
+  // })
+  // if (user) {
+  //   done(null, user)
+  // } else {
+  //   console.log('user not found during DESERIALIZE');
+  //   done(null, false)
+  // }
+  models.User.findById({
+    _id: id
+  }, function(err, user) {
+    done(null, user)
+  })
+});
 // PASSPORT MIDDLEWARE HERE
-
+app.use(passport.initialize());
+app.use(passport.session());
 // YOUR ROUTES HERE
-
+app.get('/', function(req, res) {
+  if (!req.user) {
+    console.log('failed to set user but correct UN and PW');
+    res.redirect('/login')
+  } else {
+    console.log(req.user);
+    res.render('index', {
+      user: req.user
+    });
+  }
+})
+app.get('/login', function(req, res) {
+  res.render('login');
+})
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+})
+app.get('/signup', function(req, res) {
+  res.render('signup')
+})
+app.post('/signup', function(req, res) {
+  var newUser = new models.User({
+    username: req.body.username,
+    password: hashPassword(req.body.password)
+  })
+  newUser.save(function(err) {
+    res.redirect('login')
+  })
+})
 module.exports = app;
