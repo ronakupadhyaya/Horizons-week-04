7baa073904dd9170c5b7bff916e949f06829000b
Francisco X Flores <fflores@colgate.edu>
2017-09-25 14:31:57 -0700

    Update index.js

--- day2/yelp/routes/index.js
+++ day2/yelp/routes/index.js
@@ -3,19 +3,9 @@ var router = express.Router();
 var models = require('../models/models');
 var User = models.User;
 var Follow = models.Follow;
-var Restaurant = models.Restaurant;
-var Review = models.Review;
+var Restaurant = models.Tweet;
 
-// Geocoding - uncomment these lines when the README prompts you to!
-// var NodeGeocoder = require('node-geocoder');
-// var geocoder = NodeGeocoder({
-//   provider: "google",
-//   apiKey: process.env.GEOCODING_API_KEY || "YOUR KEY HERE",
-//   httpAdapter: "https",
-//   formatter: null
-// });
-
-// THE WALL - anything routes below this are protected!
+// THE WALL - anything routes below this are protected by our passport (user must be logged in to access these routes)!
 router.use(function(req, res, next){
   if (!req.user) {
     res.redirect('/login');
@@ -24,13 +14,52 @@ router.use(function(req, res, next){
   }
 });
 
-router.post('/restaurants/new', function(req, res, next) {
+router.get('/users/', function(req, res, next) {
 
-  // Geocoding - uncomment these lines when the README prompts you to!
-  // geocoder.geocode(req.body.address, function(err, data) {
-  //   console.log(err);
-  //   console.log(data);
-  // });
+  // Gets all users
+  
+});
+
+router.get('/users/:userId', function(req, res, next) {
+
+  // Gets all information about a single user
+  
+});
+
+router.get('/tweets/', function(req, res, next) {
+
+  // Displays all tweets in the DB
+  
+});
+
+router.get('/tweets/:tweetId', function(req, res, next) {
+  
+  //Get all information about a single tweet
+  
+});
+
+router.get('/tweets/:tweetId/likes', function(req, res, next) {
+  
+  //Should display all users who like the current tweet
+
+});
+
+router.post('/tweets/:tweetId/likes', function(req, res, next) {
+  
+  //Should add the current user to the selected tweets like list (a.k.a like the tweet)
+  
+});
+
+router.get('/tweets/new', function(req, res, next) {
+  
+  //Display the form to fill out for a new tweet
+  
+});
+
+router.post('/tweets/new', function(req, res, next) {
+
+  // Handle submission of new tweet form, should add tweet to DB
+
   
 });
 
