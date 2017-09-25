737ee684367cbc35b83c21e60f195ce0a21b907c
jeff <tangj1122@gmail.com>
2017-09-25 14:13:27 -0700

    fin part1

--- day1/vault/app.js
+++ day1/vault/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 // Express setup
 var app = express();
@@ -19,11 +20,53 @@ app.use(express.static(path.join(__dirname, 'public')));
 // SESSION SETUP HERE
 
 // PASSPORT LOCALSTRATEGY HERE
+var Strategy = require('passport-local').Strategy;
+var passwordsPlain = require('./passwords.plain.json');
+
+passport.use(new Strategy(
+  function(username, password, done) {
+    var bool = false;
+    var idx;
+    passwordsPlain.passwords.forEach(function(item, index) {
+      if (item.username === username && item.password === password) {
+        bool = true;
+        idx = index;
+      }
+    });
+    if (bool) {
+      done(null, passwordsPlain.passwords[idx]);
+    }
+    else {
+      done(null, bool);
+    }
+  }
+));
+
 
 // PASSPORT SERIALIZE/DESERIALIZE USER HERE HERE
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+// passport.deserializeUser(function(user, done) {
+//   done(null, user._id);
+// });
 
 // PASSPORT MIDDLEWARE HERE
+app.use(passport.initialize());
+app.use(passport.session());
 
 // YOUR ROUTES HERE
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login'})
+);
 
 module.exports = app;
