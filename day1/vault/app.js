f03d5b0c37d415e14ce9065f148a381c5b31d4a8
jeff <tangj1122@gmail.com>
2017-09-25 20:07:36 -0700

    fin part2

--- day1/vault/app.js
+++ day1/vault/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy;
+var session = require('cookie-session');
+var passwordsPlain = require('./passwords.plain.json');
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -15,29 +17,45 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-var passwordsPlain = require('./passwords.plain.json');
 passport.use(new LocalStrategy(
-  function(username, password, done) {
+  function(username, password, cb) {
     var user = false;
     passwordsPlain.passwords.forEach(function(item, index) {
       if (item.username === username && item.password === password) {
         user = item;
       }
     });
-    done(null, user);
+    cb(null, user);
   }
 ));
 
-app.use(passport.initialize()); // TODO what do these do?
-app.use(passport.session());
+app.use(session({
+  keys: [process.env.SECRET],
+  maxAge: 1000*60*1,
+}));
 
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
+app.use(passport.initialize()); // TODO what do these do?
+app.use(passport.session()); // this MUST be run after app.use(session());
+
+
+
+// TODO why not put serialize and deserialize above app.use(passport)?
+passport.serializeUser(function(user, cb) {cb(null, user._id);});
+
+passport.deserializeUser(function(id, cb) {
+  var user = false;
+  passwordsPlain.passwords.forEach(function(item, index) {
+    if (id === item._id) {
+      user = item;
+    }
+  });
+  cb(null, user);
 });
 
+
 app.get('/', function(req, res) {
-  res.render('index');
+  if (!req.user) {res.redirect('/login');}
+  else {res.render('index', {user: req.user});}
 });
 
 app.get('/login', function(req, res) {
@@ -48,5 +66,10 @@ app.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login'})
 );
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect();
+});
 
 module.exports = app;
