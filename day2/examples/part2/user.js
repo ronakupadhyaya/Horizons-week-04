d4dd704a197bc168e0b7fab5bcf8970a062e9899
Andros <wong22@hotmail.co.uk>
2017-09-26 10:37:20 -0700

    Finish exercises

--- day2/examples/part2/user.js
+++ day2/examples/part2/user.js
@@ -3,6 +3,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var userSchemaOptions = {
+  toJSON:{
+    virtuals:true
+  }
+}
 var userSchema = new Schema({
   name: {
     first: String,
@@ -10,11 +15,37 @@ var userSchema = new Schema({
   },
   gender: String,
   birthday: Date
-},{
-  toJSON:{
-    virtuals:true
+}, userSchemaOptions
+
+);
+userSchema.methods.toggleGender = function(){
+  if(this.gender ==='male'){
+    this.gender = 'female';
+  } else{
+    this.gender = 'male';
   }
+
+  return this.gender;
+}
+
+userSchema.statics.findByName = function(name, callback){
+  this.find({'name.first': name}, callback);
+}
+var ageVirtual = userSchema.virtual('age');
+
+ageVirtual.get(function(){
+  return getAge(this.birthday);
 });
+
+ageVirtual.set(function(birthday){
+   this.birthday = birthday;
+})
+
+function getAge(birthday) {
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
 
 var User = mongoose.model('User', userSchema);
 
