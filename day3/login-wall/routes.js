2cf8ae49fcaa7bb82816d5620bcdd2862722304a
obadiar <ob.diar@gmail.com>
2017-09-27 09:43:59 -0700

    Finish morning exercise #1

--- day3/login-wall/routes.js
+++ day3/login-wall/routes.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', {
     user: req.user
   });
 });
 
-router.get('/private', function(req, res) {
+router.get('/private', function (req, res) {
   res.render('index', {
     user: req.user
   });
 });
 
-router.get('/secret', function(req, res) {
+router.get('/secret', function (req, res) {
   res.render('index', {
     user: req.user
   });
