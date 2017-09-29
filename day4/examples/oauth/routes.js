d0ea033eff7c38dc228c41ccb008f375af0d0ead
crawforde <crawforde@kenyon.edu>
2017-09-29 12:02:45 -0700

    OAuth Exercise

--- day4/examples/oauth/routes.js
+++ day4/examples/oauth/routes.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 var passport = require('passport');
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 // YOUR GITHUB STRATEGY HERE
+var GitHubStrategy = require('passport-github').Strategy;
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
+  }
+));
 
 passport.serializeUser(function(user, done) {
   done(null, user.username);
@@ -15,8 +27,14 @@ router.use(passport.initialize());
 router.use(passport.session());
 
 // YOUR GET /auth/github ENDPOINT HERE
-
+router.get('/auth/github',passport.authenticate('github'));
 // YOUR GET /auth/github/callback ENDPOINT HERE
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 router.get('/login', function(req, res) {
   res.render('login');
