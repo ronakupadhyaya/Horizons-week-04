599294fde1f4401400daa151b1bc08e820b389ab
obadiar <ob.diar@gmail.com>
2017-10-01 16:44:39 -0700

    Finish OAuth exercise

--- day5/oauth/app.js
+++ day5/oauth/app.js
@@ -11,16 +11,20 @@ app.engine('hbs', hbs);
 app.set('view engine', 'hbs');
 
 var session = require('cookie-session');
-app.use(session({keys: ['some private info']}));
+app.use(session({
+  keys: ['some private info']
+}));
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./routes'));
 
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Express started, listening to port: ', port);
 });
--- day5/oauth/routes.js
+++ day5/oauth/routes.js
@@ -5,11 +5,30 @@ var passport = require('passport');
 
 // YOUR GITHUB STRATEGY HERE
 
-passport.serializeUser(function(user, done) {
+var GitHubStrategy = require('passport-github').Strategy;
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function (accessToken, refreshToken, profile, cb) {
+    // User.findOrCreate({
+    //   githubId: profile.id
+    // }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    return cb(null, profile);
+  }
+));
+
+passport.serializeUser(function (user, done) {
   done(null, user.username);
 });
-passport.deserializeUser(function(username, done) {
-  done(null, {username: username});
+passport.deserializeUser(function (username, done) {
+  done(null, {
+    username: username
+  });
 });
 router.use(passport.initialize());
 router.use(passport.session());
@@ -18,17 +37,29 @@ router.use(passport.session());
 
 // YOUR GET /auth/github/callback ENDPOINT HERE
 
-router.get('/login', function(req, res) {
+router.get('/auth/github',
+  passport.authenticate('github'));
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/login'
+  }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+router.get('/login', function (req, res) {
   res.render('login');
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/login');
 });
 
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   if (req.user) {
     res.render('index', {
       user: req.user
